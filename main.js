4bbebb5559cd1cf81e4e02d819952a6e8cc07f3a
Orazio <orazioventrella@hotmail.it>
2021-10-22 16:10:16 +0200

    creazione variabili

---
+++ main.js
@@ -0,0 +1,29 @@
+// creare interazionecone tramite clik quando bisogna aprire o chiudere l'hamburger menu
+
+// creare le variabili prendendo le classi di riferimento
+
+const apri = document.querySelector (".fa-bars");
+
+const chiudi = document.querySelector (".close")
+
+// attivare con il clik le funzioni di apertura e chiusura
+
+apri.addEventListener("click",
+
+    function() {
+
+        
+
+    }
+
+)
+
+chiudi.addEventListener("click",
+
+    function() {
+
+
+
+    }
+
+)
